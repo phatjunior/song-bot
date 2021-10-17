1c6c24c920e2eade221b4d3283b4120bbf12d2b7
phatjunior <92602009+phatjunior@users.noreply.github.com>
2021-10-17 16:03:20 +0100

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,10 @@ class Config:
     APP_ID = int(os.environ.get("APP_ID", ""))
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    START_MSG = os.environ.get("START_MSG", "<b>Hi {},\nIam A Simple Youtube to Mp3 Downloader Bot,</b>\n\nSend me Any Songs name, I can help you with uploading that to TG")
+    START_MSG = os.environ.get("START_MSG",  "<b> Привет{},\n Я помогу тебе скачать MP3 из YouTube,</b>\n Тебе нужно всего лишь прислать мне ссылку на ролик..")
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/2a35fca576aa49de77c98.jpg")
-    OWNER = os.environ.get("OWNER", "shamilhabeeb") 
+  # OWNER = os.environ.get("OWNER", "shamilhabeeb") 
     DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/")
     msg = {}
+
+   
