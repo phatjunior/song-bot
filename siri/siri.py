a1522de453406ba733421fee33b92866e697d690
phatjunior <92602009+phatjunior@users.noreply.github.com>
2022-08-07 16:54:38 +0100

    Update siri.py

--- siri/siri.py
+++ siri/siri.py
@@ -72,7 +72,7 @@ def a(client, message):
             #     m.edit("Exceeded 30mins cap")
             #     return
 
-            performer = f'artist'
+            performer = f"апвоут бот"
             thumb_name = f'thumb{message.message_id}.jpg'
             thumb = requests.get(thumbnail, allow_redirects=True)
             open(thumb_name, 'wb').write(thumb.content)
