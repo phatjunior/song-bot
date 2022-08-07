bb725f208d64b46e2baa83433de2caa49a01ca53
phatjunior <92602009+phatjunior@users.noreply.github.com>
2022-08-07 17:23:23 +0100

    Update siri.py

--- siri/siri.py
+++ siri/siri.py
@@ -2,7 +2,7 @@
 
 from pyrogram import Client, filters
 
-import youtube_dl
+import yt_dlp
 from youtube_search import YoutubeSearch
 import requests
 
@@ -89,7 +89,7 @@ def a(client, message):
         return
     m.edit("**✅ Есть! Загрузка пошла.. Ожидай, скоро отправлю тебе.. **")
     try:
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
